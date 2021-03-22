647b9a3d8ce4599d519383eee06ef842eeecbf7a
techellipticaeducation <techellipticaeducation@gmail.com>
2021-03-22 17:27:45 +0530

    session 3 code

---
+++ src/javatraining15mar_test1/A.java
@@ -0,0 +1,25 @@
+package javatraining15mar_test1;
+
+class A{
+	public static void main(String[] args) {
+		// java A
+		System.out.println("A");
+	}
+}
+
+class B{
+	public static void main(String[] args) {
+		// java B
+		System.out.println("B");
+	}
+}
+
+class C{
+	public static void main(String[] args) {
+		// java C
+		System.out.println("C");
+	}
+}
+
+
+// Every class can have it own main method
